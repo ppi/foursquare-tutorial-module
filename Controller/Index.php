e6a263f354f10168311fda936691635998cd65c2
Paul Dragoonis <dragoonis@gmail.com>
2013-01-24 01:15:09 +0000

    Update Controller/Index.php

--- Controller/Index.php
+++ Controller/Index.php
@@ -5,21 +5,18 @@ use FourSquareModule\Controller\Shared as SharedController;
 
 class Index extends SharedController
 {
-
     public function indexAction()
     {
         return $this->render('FourSquareModule:index:index.html.php');
     }
-
+     
+    // Get the lat/long from the route, get venues from the foursquare.handler, and output them as json
     public function getVenuesAction()
     {
-
-        $lat      = $this->getRouteParam('lat');
-        $lng      = $this->getRouteParam('lng');
-        $handler  = $this->getService('foursquare.handler');
-        $venues   = $handler->getVenues($lat, $lng);
-
-        echo $venues;
+        $lat       = $this->getRouteParam('lat');
+        $lng       = $this->getRouteParam('lng');
+        $handler   = $this->getService('foursquare.handler');
+        $venues    = $handler->getVenues($lat, $lng);
+        return json_encode($venues, true);
     }
-
 }
